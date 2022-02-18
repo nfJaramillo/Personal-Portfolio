a4b59c7655700635ce88c1812f2e1babe3abe665
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-02-17 19:03:22 -0500

    Interests v1.1
    Interests added and general fixes

--- main.js
+++ main.js
@@ -4,5 +4,14 @@ $(document).ready(function () {
     $('.navbar-nav>a').on('click', function(){
         $('.navbar-collapse').collapse('hide');
     });
+
+    $( ".mx-auto .nav-item" ).bind( "click", function(event) {
+       // event.preventDefault();
+        var clickedItem = $( this );
+        $( ".mx-auto .nav-item" ).each( function() {
+            $( this ).removeClass( "active" );
+        });
+        clickedItem.addClass( "active" );
+    });
     
   });
